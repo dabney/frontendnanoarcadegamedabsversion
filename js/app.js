c5c193682daf1da1beaaf0d9bd49eb216091ed91
Dabney Blum <dabneyblum@gmail.com>
2014-11-18 16:11:21 -0600

    collisions

--- js/app.js
+++ js/app.js
@@ -8,6 +8,7 @@ var TILEHEIGHT = 83;
 var IMAGEWIDTH = 101;
 var IMAGEHEIGHT = 171;
 var SPEEDCONSTANT = 80;
+var NUMENEMIES = 3;
 
 // Enemies our player must avoid
 var Enemy = function() {
@@ -41,7 +42,6 @@ Enemy.prototype.update = function(dt) {
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
-
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
@@ -54,9 +54,11 @@ var Player = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/char-cat-girl.png';
-
+this.column = 3;
   this.x = CANVASWIDTH/2 - TILEWIDTH/2;
+  this.row = 6;
  this.y = CANVASHEIGHT - IMAGEHEIGHT - TILEHEIGHT/2;
+ this.alive = true;
 
 }
 
@@ -66,13 +68,29 @@ Player.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
    //check for collision?
+       var i;
+
+       for (i = 0; i < NUMENEMIES; i++) {
+    if (collisionDetected(player.x, player.y, allEnemies[i].x, allEnemies[i].y, 100)) {
+    player.alive = false;
+    }
+}
 
 }
 
 // Draw the player on the screen, required method for game
 Player.prototype.render = function() {
   //  console.log('this.sprite:' + this.sprite + ', ' + Resources.get(this.sprite));
+  if (player.alive) {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+  else {
+    ctx.save()
+    ctx.globalAlpha = .3
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        ctx.restore();
+
+  }
 }
 
 // handle keystroke input
@@ -85,27 +103,35 @@ Player.prototype.handleInput = function(direction) {
                 tmpX = player.x - TILEWIDTH;
                 if (!offCanvasEdge(tmpX, player.y)) {
                     player.x = tmpX;
+                    player.column = player.column - 1;
                 }
         break; 
         case 'right':
                        tmpX = player.x + TILEWIDTH;
                 if (!offCanvasEdge(tmpX, player.y)) {
                     player.x = tmpX;
+                player.column = player.column + 1;
+
                 }
         break;
         case 'up':
                        tmpY = player.y - TILEHEIGHT;
                 if (!offCanvasEdge(player.x, tmpY)) {
                     player.y = tmpY;
+                    player.row = player.row + 1;
+
                 }
             break;
         case 'down':
                        tmpY = player.y + TILEHEIGHT;
                 if (!offCanvasEdge(player.x, tmpY)) {
                     player.y = tmpY;
+                    player.row = player.row - 1;
+
                 }
             break;
     }
+
 }
 
 var offCanvasEdge = function(x, y) {
@@ -120,6 +146,17 @@ var offCanvasEdge = function(x, y) {
     }
     else {
         console.log('on Canvas');
+        return(false);
+    }
+}
+
+var collisionDetected = function(x1, y1, x2, y2, distance) {
+ //   console.log('checking for collision', Math.abs(x1-x2), Math.abs(y1-y2));
+    if (Math.abs(x1 - x2) < distance && Math.abs(y1 - y2) < distance) {
+        console.log('collision detected');
+        return(true);
+    }
+    else {
         return(false);
     }
 }
